4833a81868b3003a2ed0899a8d0e4513a05fb83a
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-09-29 21:52:48 +0200

    Add swagger

--- src/main/java/dk/fitfit/oneliner/configuration/CorsConfiguration.java
+++ src/main/java/dk/fitfit/oneliner/configuration/CorsConfiguration.java
@@ -2,11 +2,9 @@ package dk.fitfit.oneliner.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
